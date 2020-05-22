8978bfae5802a954db87731d1fde1ba054a39b28
rohitsengar001 <39033056+rohitsengar001@users.noreply.github.com>
2020-05-22 09:55:12 +0530

    Add files via upload

---
+++ php coursera/WEEK_5/assignment_week5/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <titile>ROHIT PHP</titile>
+</head>
+<body>
+    <h1>ROHIT PHP</h1>
+    <pre>&nbsp&nbsp&nbsp&nbspROHIT</pre>
+    <?
+     print hash('sha256', 'Charles Severance');
+    ?>
+</body>
+</html>
